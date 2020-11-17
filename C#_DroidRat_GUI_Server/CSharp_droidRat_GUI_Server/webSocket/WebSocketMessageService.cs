4b5cab126c99f6eb107d9f46e18d6d551a320b6c
JE-Chen <zenmailman@gmail.com>
2020-11-18 00:13:38 +0800

    add C# GUI
    add C# GUI

---
+++ C#_DroidRat_GUI_Server/CSharp_droidRat_GUI_Server/webSocket/WebSocketMessageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace WebSocket_JE
+{
+    public class WebSocket : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            var data = e.Data;
+            Console.WriteLine(e.Data);
+            Send(e.Data);
+        }
+    }
+}
---
+++ C#_DroidRat_GUI_Server/CSharp_droidRat_GUI_Server/webSocket/WebSocketServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WebSocketSharp.Server;
+using WebSocket_JE;
+
+namespace WesocketServer_JE
+{
+    public class WebSocketServerJE
+    {
+        public WebSocketServerJE(String url1, String url2)
+        {
+            var webSocketServer = new WebSocketServer(url1);
+            webSocketServer.AddWebSocketService<WebSocket>(url2);
+            webSocketServer.Start();
+            while (true)
+            {
+                String command = Console.ReadLine();
+                if (command.Equals("exit"))
+                    break;
+            }
+        }
+    }
+}
