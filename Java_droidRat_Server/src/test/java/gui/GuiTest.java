daabc8ed29f52385e2cec8cdc1c5c49f1783eddc
JE-Chen <zenmailman@gmail.com>
2020-11-22 03:59:18 +0800

    add GUI Junit test
    add GUI Junit test

--- Java_droidRat_Server/src/test/java/gui/GuiTest.java
+++ Java_droidRat_Server/src/test/java/gui/GuiTest.java
@@ -1,7 +1,19 @@
 package test.java.gui;
 
 import com.je_chen.droidRat.gui.callgui.CallGui;
+import com.je_chen.droidRat.gui.cameragui.CameraGui;
+import com.je_chen.droidRat.gui.infogui.InfoGui;
+import com.je_chen.droidRat.gui.installgui.InstallGui;
+import com.je_chen.droidRat.gui.locationgui.LocationGui;
+import com.je_chen.droidRat.gui.mailgui.MailGui;
 import com.je_chen.droidRat.gui.maingui.MainGui;
+import com.je_chen.droidRat.gui.playgui.PlayGui;
+import com.je_chen.droidRat.gui.runappgui.RunAppGui;
+import com.je_chen.droidRat.gui.sensorgui.SensorGui;
+import com.je_chen.droidRat.gui.smsgui.SMSGui;
+import com.je_chen.droidRat.gui.toastgui.ToastGui;
+import com.je_chen.droidRat.gui.vibratorgui.VibratorGui;
+import com.je_chen.droidRat.gui.webgui.WebGui;
 import org.junit.jupiter.api.Test;
 
 class GuiTest {
@@ -14,5 +26,65 @@ class GuiTest {
     @Test
     void getCallGuiInstance(){
         CallGui.getInstance("Test");
+    }
+
+    @Test
+    void getCameraGuiInstance(){
+        CameraGui.getInstance("Test");
+    }
+
+    @Test
+    void getInfoGuiInstance(){
+        InfoGui.getInstance("Test");
+    }
+
+    @Test
+    void getInstallGuiInstance(){
+        InstallGui.getInstance("Test");
+    }
+
+    @Test
+    void getLocationGuiInstance(){
+        LocationGui.getInstance("Test");
+    }
+
+    @Test
+    void getMailGuiInstance(){
+        MailGui.getInstance("Test");
+    }
+
+    @Test
+    void getPlayGuiInstance(){
+        PlayGui.getInstance("Test");
+    }
+
+    @Test
+    void getRunAppGuiInstance(){
+        RunAppGui.getInstance("Test");
+    }
+
+    @Test
+    void getSensorGuiInstance(){
+        SensorGui.getInstance("Test");
+    }
+
+    @Test
+    void getSMSGuiInstance(){
+        SMSGui.getInstance("Test");
+    }
+
+    @Test
+    void getToastGuiInstance(){
+        ToastGui.getInstance("Test");
+    }
+
+    @Test
+    void getVibratorGuiInstance(){
+        VibratorGui.getInstance("Test");
+    }
+
+    @Test
+    void getWebGuiInstance(){
+        WebGui.getInstance("Test");
     }
 }
